6a012ce022d0cab2862e56c19f6b203cfa361114
Piuymi12 <piyumifernando1996@gmail.com>
2020-08-17 11:29:04 +0530

    Gallery,spa,feedback added

---
+++ app/src/main/java/com/example/gallery/Feedback.java
@@ -0,0 +1,15 @@
+package com.example.gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+    }
+}
