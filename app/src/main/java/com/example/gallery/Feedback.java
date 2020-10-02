314827788fbd2bdc3b54ed4a3e7b86f94a58d242
Piuymi12 <piyumifernando1996@gmail.com>
2020-10-02 22:27:12 +0530

    Gallery,spa,feedback added with navigation

--- app/src/main/java/com/example/gallery/Feedback.java
+++ app/src/main/java/com/example/gallery/Feedback.java
@@ -1,10 +1,36 @@
 package com.example.gallery;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.media.Rating;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Feedback extends AppCompatActivity {
+
+    Button butDelete, butShow, butUpdate, butSend;
+
+    EditText txtName, txtEmail, txtFeedback;
+    RatingBar rating;
+    DatabaseReference dbRef;
+    CusFeedback cus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
